9dfa1c82a7301b3feae9169c110746ed7317540e
Amama-Fatima <princessamama321@gmail.com>
2023-09-16 11:33:21 +0500

    trying to see why vercel is not uploading my images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    assetPrefix: process.env.BASE_PATH || '',
+    publicRuntimeConfig: {
+      basePath: process.env.BASE_PATH || '',
+    },
+  
 }
 
 module.exports = nextConfig
